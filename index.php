65997d9d295733f6cb0c871fafd0b28aa0bb8602
Candra <liaocandra888@gmail.com>
2021-05-29 15:52:31 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Latihan Facebook</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<script type="text/javascript" src="js/bootstrap.php"></script>
+	</head>
+	<body>
+	</body>
+</html>
